bd3a06e79fb25ae581b7fe0311d534c7bb7e671f
Gene H Li <ghl234@nyu.edu>
2019-09-06 20:39:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -19,7 +19,7 @@ def introduction_with_language_optional(name, language*)
   if (language == NIL) puts "Hi, my name is #{name} and I am learning to program in Ruby"
 
   else puts "Hi, my name is #{name} and I am learning to program in #{language}"
-
+end
 
 
 
